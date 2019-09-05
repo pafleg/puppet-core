d994d97840a2b91bfffb3904000afe60639d24d3
pafleg <33604023+pafleg@users.noreply.github.com>
2019-09-05 23:35:32 +0300

    Update spa.pp

--- modules/users/manifests/spa.pp
+++ modules/users/manifests/spa.pp
@@ -3,22 +3,23 @@ class users::spa (
   $hugeperc = 52,
   $hugepagenr = $ram*$hugeperc/100/2,
 ){
-  group { 'spa':
-    ensure => 'present',
-    gid    => 1000,
-  } ->
+  # group { 'spa':
+  #   ensure => 'present',
+  #   gid    => 1000,
+  # } ->
 
-  user { 'spa':
-    ensure             => 'present',
-    comment            => "2 Selin Pavel ${hugepagenr}",
-    gid                => 1000,
-    groups             => ['wheel', 'spa'],
-    home               => '/home/spa',
-    password           => '$6$BQKm442WHVUl1nlY$zs7oIS/NmYB3rbxcsTI7TmSBWyvRgpgPyfmbnhazfxV/CI32q0DIaRmUtIOG9Kh5eNsSDgLKFxXca9taDNYh61',
-    password_max_age   => 99999,
-    password_min_age   => 0,
-    password_warn_days => 7,
-    shell              => '/bin/bash',
-    uid                => 1000,
-  }
+  # user { 'spa':
+  #   ensure             => 'present',
+  #   comment            => "2 Selin Pavel ${hugepagenr}",
+  #   gid                => 1000,
+  #   groups             => ['wheel', 'spa'],
+  #   home               => '/home/spa',
+  #   password           => '$6$BQKm442WHVUl1nlY$zs7oIS/NmYB3rbxcsTI7TmSBWyvRgpgPyfmbnhazfxV/CI32q0DIaRmUtIOG9Kh5eNsSDgLKFxXca9taDNYh61',
+  #   password_max_age   => 99999,
+  #   password_min_age   => 0,
+  #   password_warn_days => 7,
+  #   shell              => '/bin/bash',
+  #   uid                => 1000,
+  # }
+  notify {"Running!":}
 }
